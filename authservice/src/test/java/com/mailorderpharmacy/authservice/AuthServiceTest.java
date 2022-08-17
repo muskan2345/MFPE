4623fc2d15618592c0588b8d699fba9c96783e2f
DELL <muskan2105gupta@gmail.com>
2022-08-17 17:58:58 +0530

    First commit

---
+++ authservice/src/test/java/com/mailorderpharmacy/authservice/AuthServiceTest.java
@@ -0,0 +1,19 @@
+package com.mailorderpharmacy.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+ class AuthServiceTest {
+
+	@Test
+	 void main() {
+		AuthService.main(new String[] {});
+		assertTrue(true);
+	}
+}
+
